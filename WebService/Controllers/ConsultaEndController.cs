a84b66a5e17c5ae96aa069c424dea817b5d8b9e8
ggsb1997 <gabrielsahinabier@gmail.com>
2019-11-30 16:18:14 -0300

    Commit

---
+++ WebService/Controllers/ConsultaEndController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using WebService.Models;
+
+//Gabriel Giuseppe Sahina Bier 1726478
+
+namespace WebService.Controllers
+{
+    public class ConsultaEndController : Controller
+    {
+        private readonly Context _context;
+        public ConsultaEndController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.ConsultaEnd.ToList());
+        }
+
+        public IActionResult ConsultaEnd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConsultaEnd(string cep)
+        {
+            WebClient client = new WebClient();
+            string json = client.DownloadString("https://viacep.com.br/ws/" + cep + "/json/");
+            ConsultaEnd cidade = JsonConvert.DeserializeObject<ConsultaEnd>(json);
+
+            _context.Add(cidade);
+            _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebService/Controllers/EnderecoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebService.Models;
+
+namespace WebService.Controllers
+{
+    [Route("api")]
+    public class EnderecosController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public EnderecosController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Endereco/Enderecos")]
+        public IEnumerable<ConsultaEnd> GetAll()
+        {
+            List<ConsultaEnd> enderecos = _context.ConsultaEnd.ToList();
+            return enderecos;
+        }
+
+        [HttpGet("Endereco/Enderecos/{cep}")]
+        public IEnumerable<ConsultaEnd> GetPorCep(string cep)
+        {
+            var endereco = _context.ConsultaEnd.Where(s => s.Cep == cep);
+            return endereco;
+        }
+
+        [HttpGet("Endereco/EnderecosPorEstado/{uf}")]
+        public IEnumerable<ConsultaEnd> GetPorEstado(string uf)
+        {
+            List<ConsultaEnd> enderecos = _context.ConsultaEnd.Where(s => s.Uf == uf).ToList();
+            return enderecos;
+        }
+    }
+}
